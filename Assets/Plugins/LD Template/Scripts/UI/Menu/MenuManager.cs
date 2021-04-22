d41dac74ba03cd4b21c14c2d0dd26805815bdd10
Shark Desktop (Owen) <owensilva1282@gmail.com>
2021-04-22 19:58:47 -0400

    Getting setup
    The hype for LD is real:
    Imported a bunch of standard libraries like TextMeshPro, implemented my own utility code library and setup a nice looking unity webGL template forked from

---
+++ Assets/Plugins/LD Template/Scripts/UI/Menu/MenuManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles common main menu related button functions
+/// </summary>
+public class MenuManager : MonoBehaviour
+{
+    //See LevelLoader.cs for loading stuff
+
+    public void Quit () => Application.Quit();
+}
