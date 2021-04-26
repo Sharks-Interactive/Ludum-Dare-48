c0e7411258936557b2def1865062397ab62aa1a8
Owen <sharksinteractive@gmail.com>
2021-04-26 09:33:09 -0400

    Update FinalSceneHandler.cs

--- Assets/Scripts/FinalSceneHandler.cs
+++ Assets/Scripts/FinalSceneHandler.cs
@@ -8,10 +8,13 @@ public class FinalSceneHandler : MonoBehaviour {
   public GameObject[] Doors = new GameObject[3];
   public GameObject ExplosionEffects;
   public GameObject GameOverScreenThing;
-
+  public Image[] DoorObjects = new Image[6];
+  public Image OverallSlider;
+  
   [Header("Balance Options")]
   public float[] DoorValueIncrease = new float[3];
   public float[] DoorValueDecrease = new float[3];
+  public float HackTime = 30;
 
   //Cache
   private float[] _doorProgress = new float[3];
@@ -25,11 +28,34 @@ public class FinalSceneHandler : MonoBehaviour {
     return _doorProgress[t_computerNum];
   }
 
+  void Start ()
+  {
+    StartCoroutine(HackFacility()); 
+  }
+  
+  public Ienumerator HackFacility ()
+  {
+    yield return new WaitForSeconds(0.2f);
+    OverallSlider.fill = 1 / HackTime;
+    HackTime -= 0.2;
+    
+    if (HackTime <= 0)
+    {
+      ExplosionEffects.SetActive(true);
+      return;
+    }
+    
+    StartCoroutine(HackFacility());
+  }
+  
   void Update ()
   {
     for (int z; z < _doorProgress.Length; z++)
     {
+      if (_doorProgress[z] <= 0) break;
       _doorProgress[z] -= DoorValueDecrease[z] * Time.deltaTime;
+      DoorObjects[z].fill = _doorProgress[z];
+      DoorObjects[z + 3].fill = _doorProgress[z];
       
       GameOverScreenThing.SetActive(_doorProgress[z] <= 0);
     }
